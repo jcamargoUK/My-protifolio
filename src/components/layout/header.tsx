68bcd9b99d3ebdb6109040a25288fa59f2b5d647
Julio Camargo <jcamargo1975@gmail.com>
2025-07-11 13:20:15 +0000

    Nearly there, Give the navbar a little more padding on the top. Reduce a

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -22,12 +22,12 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center justify-between">
+      <div className="container flex h-20 max-w-screen-2xl items-center justify-between">
         <Link href="/" className="flex items-center gap-2" prefetch={false}>
           <Code className="h-6 w-6 text-primary" />
           <span className="font-headline text-lg font-bold">Julio Camargo</span>
         </Link>
-        <nav className="hidden md:flex items-center gap-4 lg:gap-6 text-sm font-medium">
+        <nav className="hidden md:flex items-center gap-2 lg:gap-4 text-sm font-medium">
           {navLinks.map((link) => (
             <Link
               key={link.href}
